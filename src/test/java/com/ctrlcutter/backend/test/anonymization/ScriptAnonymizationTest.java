1c49de8f3c8b55dc3c652e5dd204c0116998df11
D45Hub <denis.thiessen@gmx.de>
2022-04-03 11:01:46 +0200

    Added Text Anonymization test.

--- src/test/java/com/ctrlcutter/backend/test/anonymization/ScriptAnonymizationTest.java
+++ src/test/java/com/ctrlcutter/backend/test/anonymization/ScriptAnonymizationTest.java
@@ -13,12 +13,27 @@ import com.ctrlcutter.backend.service.anonymization.ScriptAnonymizationService;
 public class ScriptAnonymizationTest {
 
     @Test
-    public void testScriptAnonymization() {
+    public void testRunScriptAnonymization() {
         String runURL = "https://youtube.com";
         BasicScriptDTO exampleDTO = new BasicScriptDTO("win", "RUN", "c", new String[] {"CTRL"}, new String[] {runURL});
         AnonymizedScriptDTO anonymizedScriptDTO = ScriptAnonymizationService.anonymizeScriptDTO(exampleDTO);
 
         assertFalse(runURL.equals(anonymizedScriptDTO.getParameters()[0]));
+        assertEquals(anonymizedScriptDTO.getParameters()[0], "https://ctrlcutter.com");
+        assertEquals(exampleDTO.getCommand(), anonymizedScriptDTO.getCommand());
+        assertEquals(exampleDTO.getKey(), anonymizedScriptDTO.getKey());
+        assertEquals(exampleDTO.getModifierKeys()[0], anonymizedScriptDTO.getModifierKeys()[0]);
+        assertTrue(anonymizedScriptDTO.getModifierKeys().length == 1);
+    }
+
+    @Test
+    public void testSendScriptAnonymization() {
+        String textOutput = "Some Secret Text";
+        BasicScriptDTO exampleDTO = new BasicScriptDTO("win", "SEND", "f", new String[] {"CTRL"}, new String[] {textOutput});
+        AnonymizedScriptDTO anonymizedScriptDTO = ScriptAnonymizationService.anonymizeScriptDTO(exampleDTO);
+        
+        assertFalse(textOutput.equals(anonymizedScriptDTO.getParameters()[0]));
+        assertEquals(anonymizedScriptDTO.getParameters()[0], "Anonymous Sample Text");
         assertEquals(exampleDTO.getCommand(), anonymizedScriptDTO.getCommand());
         assertEquals(exampleDTO.getKey(), anonymizedScriptDTO.getKey());
         assertEquals(exampleDTO.getModifierKeys()[0], anonymizedScriptDTO.getModifierKeys()[0]);
