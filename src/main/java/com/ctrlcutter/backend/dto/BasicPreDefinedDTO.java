356fe8fb8a5c85fe24d57c80f2098bcb2480675b
Lorenz Seufert <lorenzseufert@gmail.com>
2022-04-10 19:23:53 +0200

    added DTO for basic and predefined get request

---
+++ src/main/java/com/ctrlcutter/backend/dto/BasicPreDefinedDTO.java
@@ -0,0 +1,36 @@
+package com.ctrlcutter.backend.dto;
+
+import java.util.List;
+
+import com.ctrlcutter.backend.persistence.model.BasicScript;
+import com.ctrlcutter.backend.persistence.model.PreDefinedScript;
+
+public class BasicPreDefinedDTO {
+
+    private List<BasicScript> basicScripts;
+
+    private List<PreDefinedScript> preDefinedScripts;
+
+    public BasicPreDefinedDTO() {}
+
+    public BasicPreDefinedDTO(List<BasicScript> basicScripts, List<PreDefinedScript> preDefinedScripts) {
+        this.basicScripts = basicScripts;
+        this.preDefinedScripts = preDefinedScripts;
+    }
+
+    public List<BasicScript> getBasicScripts() {
+        return this.basicScripts;
+    }
+
+    public void setBasicScripts(List<BasicScript> basicScripts) {
+        this.basicScripts = basicScripts;
+    }
+
+    public List<PreDefinedScript> getPreDefinedScripts() {
+        return this.preDefinedScripts;
+    }
+
+    public void setPreDefinedScripts(List<PreDefinedScript> preDefinedScripts) {
+        this.preDefinedScripts = preDefinedScripts;
+    }
+}
